66af4771bb2db784c2be3988608180ed84eeb5f7
RaistlinJ <raistlinJ@users.noreply.github.com>
2026-04-03 16:31:36 -0600

    Initial commit of Mario Kart theme

---
+++ vite.config.js
@@ -0,0 +1,43 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import copy from 'rollup-plugin-copy';
+
+export default defineConfig({
+  build: {
+    outDir: 'static',
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'assets/js/index.js'),
+        challenges: resolve(__dirname, 'assets/js/challenges.js'),
+        scoreboard: resolve(__dirname, 'assets/js/scoreboard.js'),
+        settings: resolve(__dirname, 'assets/js/settings.js'),
+        setup: resolve(__dirname, 'assets/js/setup.js'),
+        page: resolve(__dirname, 'assets/js/page.js'),
+        teams: resolve(__dirname, 'assets/js/teams.js'),
+        users: resolve(__dirname, 'assets/js/users.js'),
+        notifications: resolve(__dirname, 'assets/js/notifications.js'),
+      },
+      output: {
+        entryFileNames: `[name].js`,
+        chunkFileNames: `[name].js`,
+        assetFileNames: `[name].[ext]`,
+      },
+    },
+  },
+  plugins: [
+    copy({
+      targets: [
+        { src: 'assets/img/*', dest: 'static/img' },
+        { src: 'node_modules/@fortawesome/fontawesome-free/webfonts/*', dest: 'static/webfonts' },
+      ],
+      hook: 'writeBundle'
+    })
+  ],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "assets/scss/variables";`
+      }
+    }
+  }
+});
